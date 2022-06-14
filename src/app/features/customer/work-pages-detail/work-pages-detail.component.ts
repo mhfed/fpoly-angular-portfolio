33065cbd058111195e3d5a9521eaab2d5e98266b
Hieu Minh <nmhieu04091999@gmail.com>
2022-06-14 08:02:29 +0700

    fix project detail

--- src/app/features/customer/work-pages-detail/work-pages-detail.component.ts
+++ src/app/features/customer/work-pages-detail/work-pages-detail.component.ts
@@ -1,15 +1,23 @@
+import { ProjectElement } from './../../../shared/models/projects.model';
+import { ProjectsService } from './../../../services/projects.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-work-pages-detail',
   templateUrl: './work-pages-detail.component.html',
-  styleUrls: ['./work-pages-detail.component.css']
+  styleUrls: ['./work-pages-detail.component.css'],
 })
 export class WorkPagesDetailComponent implements OnInit {
-
-  constructor() { }
+  project!: ProjectElement;
+  constructor(
+    private router: ActivatedRoute,
+    private ProjectsService: ProjectsService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.ProjectsService.getProject(id).subscribe((data) => {
+      this.project = data;
+    });
   }
-
 }
